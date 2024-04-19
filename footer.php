3b7519d3455704608d38d0f403b52e1c8a6dbc8e
Heshhoax <heshanperera491@gmail.com>
2024-04-19 20:53:55 +0530

    updated footer

--- footer.php
+++ footer.php
@@ -1,29 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-        <footer>
-<table class="desing"  width="100%">
-	<tr style="border:0px">
-		<th style="font-family:Geneva"><h3 style="font-family:sans-serif; font-size:20px;">Follow us on</th></h3>
-	</tr>
-	<tr> <th>
-    <a href="https://www.facebook.com/"><img src="Images/ICONS/FB2.png" width=50" height="50"></a>
-	<a href="https://www.twitter.com/"><img src="Images/ICONS/Twitter2.png" width=50" height="50"></a>
-	<a href="https://www.instagram.com/"><img src="Images/ICONS/Insta2.png" width=50" height="50"></a>
-	<a href="https://www.googleplus.com/"><img src="Images/ICONS/G+2.png" width=50" height="50"></a>
-	<br>
-	<a href="reviews.html"><img src="Images/ICONS/rw2.png" width="170" height="60"></a>
-<br><center><h3 style="font-family:sans-serif"> Terms and conditions</center></h3>
-<p style="font-size:13px; font-family:Arial;">The following are terms of a legal agreement butween you and the Sri Lanka Tourism Promotion Bunau. By accessing, browsing, or using this Website, you acknowledge that you have read, understood, and agee to be bound by these terms and to comply with all applicable laws and regulationsf you do not agree to these terms, please do not use this Website. SLTPB may, without notice to you, at any time rvse these Tens of Use and any other information contained in this Website by updating this posting SLUPB may also make improvemets or changes in the products, survices, or programs described in this sile at any time without notice.</p>
-<center><h4 style="font-family:sans-serif">@ 2020 ALL Rights Rerved by "Travel Net"</a></h4></center> 
-	
-</th>
-</table>
+<footer>
+    <p>&copy; <?php echo date("Y"); ?> Your Website Name. All rights reserved.</p>
 </footer>
-</body>
-</html>
--- welcome.php
+++ welcome.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>welcome</title>
+    <title>Welcome</title>
 </head>
 <body>
-    <h1>Welcome EDUpro Team</h1>   
-    <?php
-     required_once['footer.php']; 
-     ?>
+    <h1>Welcome EDUpro Team</h1>
+    <?php include 'footer.php';?>
 </body>
 </html>
