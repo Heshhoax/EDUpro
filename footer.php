646ef7e661c4dde1dd080f06fe93d3b14cafd9ce
Heshhoax <139737549+Heshhoax@users.noreply.github.com>
2024-04-20 15:31:41 +0530

    updated footer

--- footer.php
+++ footer.php
@@ -6,20 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
 </head>
-<body>
-	
-        <footer>
+<body>	
+ <footer>
 <table class="desing"  width="100%">
 	<tr style="border:0px">
 		<th style="font-family:Geneva"><h3 style="font-family:sans-serif; font-size:20px;">Follow us on</th></h3>
 	</tr>
 	<tr> <th>
-    <a href="https://www.facebook.com/"><img src="" width="50" height="50"></a>
-	<a href="https://www.twitter.com/"><img src="Images/ICONS/Twitter2.png" width="50" height="50"></a>
+    <a href="https://www.facebook.com/"><img src="images/square-facebook.svg" width="50" height="50"></a>
+	<a href="https://www.twitter.com/"><img src="images/square-twitter.svg" width="50" height="50"></a>
 	<a href="https://www.instagram.com/"><img src="images/square-instagram.svg" width="50" height="50"></a>
-	<a href="https://www.googleplus.com/"><img src="Images/ICONS/G+2.png" width="50" height="50"></a>
+	<a href="https://www.googleplus.com/"><img src="images/square-google-plus.svg" width="50" height="50"></a>
 	<br>
-	<a href="reviews.html"><img src="Images/ICONS/rw2.png" width="170" height="60"></a>
+	<a href="reviews.html"><img src="images/mainlogo.jpg" width="170" height="60"></a>
 <br><center><h3 style="font-family:sans-serif"> Terms and conditions</center></h3>
 <footer>
     <p>&copy; <?php echo date("Y"); ?> EDUpro All rights reserved.</p>
@@ -29,5 +28,6 @@
 </th>
 </table>
 </footer>
+<hr>
 </body>
 </html>
