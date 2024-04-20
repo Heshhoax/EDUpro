eb41d216fe1c9f3d0b1174668ba92373cb14e9c3
Heshhoax <139737549+Heshhoax@users.noreply.github.com>
2024-04-20 13:33:31 +0530

    update

--- footer.php
+++ footer.php
@@ -7,17 +7,17 @@
     <title></title>
 </head>
 <body>
-	<hr>
+	
         <footer>
 <table class="desing"  width="100%">
 	<tr style="border:0px">
 		<th style="font-family:Geneva"><h3 style="font-family:sans-serif; font-size:20px;">Follow us on</th></h3>
 	</tr>
 	<tr> <th>
-    <a href="https://www.facebook.com/"><img src="" width=50" height="50"></a>
-	<a href="https://www.twitter.com/"><img src="Images/ICONS/Twitter2.png" width=50" height="50"></a>
-	<a href="https://www.instagram.com/"><img src="images/square-instagram.svg" width=50" height="50"></a>
-	<a href="https://www.googleplus.com/"><img src="Images/ICONS/G+2.png" width=50" height="50"></a>
+    <a href="https://www.facebook.com/"><img src="" width="50" height="50"></a>
+	<a href="https://www.twitter.com/"><img src="Images/ICONS/Twitter2.png" width="50" height="50"></a>
+	<a href="https://www.instagram.com/"><img src="images/square-instagram.svg" width="50" height="50"></a>
+	<a href="https://www.googleplus.com/"><img src="Images/ICONS/G+2.png" width="50" height="50"></a>
 	<br>
 	<a href="reviews.html"><img src="Images/ICONS/rw2.png" width="170" height="60"></a>
 <br><center><h3 style="font-family:sans-serif"> Terms and conditions</center></h3>
