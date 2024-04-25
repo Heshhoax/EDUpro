eb0ac50ff789d3e59fbe402ab634e133e01524ef
inuka18 <167287575+inuka18@users.noreply.github.com>
2024-04-25 19:01:43 +0530

    jj

--- course.php
+++ course.php
@@ -27,24 +27,24 @@
             <div class="packoff" id="side7">          
                  <img src="images/course1.jpg" width="200px" height="200px">
                  <p>Platinum Credit Cards often include exclusive travel benefits like airport lounge access, concierge services, travel insurance, and lost baggage reimbursement. Platinum Credit Cards typically offer purchase protections like extended warranties, returns, refunds, and product guarantees.</p>
-                 <button class="buy" id="btn-buy">Course 1</button>
+                 <button class="buy1" id="btn-buy1">Course 1</button>
             </div>
             <div class="packoff" id="side7">  
                  <img src="images/course2.jpg " width="200px" height="200px">
                  <p>Gold is seen as a hedge against inflation and a store of value through market ups and downs. Investors can hold physical gold directly in the form of coins, bullion, or jewelry; or indirectly via mutual funds, exchange-traded funds (ETFs), gold derivatives, or gold-mining stocks.</p>
-                 <button class="buy" id="btn-buy">Course 2</button>
+                 <button class="buy1" id="btn-buy1">Course 2</button>
                  
             </div>
             <div class="packoff" id="side7">  
                  <img src="images/course3.jpg " width="200px" height="200px">
                  <p>Silver is a relatively soft, shiny metal. It tarnishes slowly in air as sulfur compounds react with the surface forming black silver sulfide. Sterling silver contains 92.5% silver. The rest is copper or some other metal.</p>
-                 <button class="buy" id="btn-buy">Course 3</button>    
+                 <button class="buy1" id="btn-buy1">Course 3</button>    
             </div>
 
             <div class="packoff" id="side7">  
                 <img src="images/course4.jpeg " width="200px" height="200px">
                 <p>Silver is a relatively soft, shiny metal. It tarnishes slowly in air as sulfur compounds react with the surface forming black silver sulfide. Sterling silver contains 92.5% silver. The rest is copper or some other metal.</p>
-                <button class="buy" id="btn-buy">Course 4</button>    
+                <button class="buy1" id="btn-buy1">Course 4</button>    
            </div>
          </div>
 
