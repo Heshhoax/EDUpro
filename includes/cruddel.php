454b2241366c0b99be9d9b9a51ddb121c14ed9d2
Heshhoax <139737549+Heshhoax@users.noreply.github.com>
2024-04-28 15:19:00 +0530

    crud by heshan

---
+++ includes/cruddel.php
@@ -0,0 +1,15 @@
+<?php
+
+if(isset($_GET["ID"]))
+    $deluser = $_GET['ID'];
+
+    require_once('database.php');
+    
+    $query = "DELETE FROM userinfo WHERE ID='$deluser'";
+    
+    if (mysqli_query($conn, $query)) {
+        header("location:../Heshancrud.php");
+    } else {
+        echo "Error deleting user: " . mysqli_error($conn);
+    }
+
