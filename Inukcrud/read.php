1df13781865f1a3d136e5baf17c91ca76bc96b81
inuka18 <167287575+inuka18@users.noreply.github.com>
2024-05-07 00:58:12 +0530

    dn balanna

---
+++ Inukcrud/read.php
@@ -0,0 +1,52 @@
+<style>
+    table{
+    border-collapse: collapse;
+    width: 100%;
+}
+th,td{
+    padding:8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+th{
+    background-color: #800080;
+    color: white;
+}
+</style>
+<?php
+
+echo"
+<table>
+<thead>
+<tr>
+<th>C_ID</th>
+<th>Title</th>
+<th>Description</th>
+<th>Image</th>
+</tr>
+</thead>
+</tbody>
+";
+
+
+require_once('database1.php');
+
+$sql="SELECT * FROM course";
+$result=$conn->query($sql);
+
+if(!$result){
+    die('connection error');
+}
+while($row=$result->fetch_assoc()){
+  
+    echo"
+<tr>
+<td>{$row['C_ID']}</td>
+<td>{$row['Title'] }</td>
+<td>{$row['Description']}</td>
+<td>{$row['Image']}</td>
+<td><a href='update.php?C_ID={$row['C_ID']}'>EDIT</a></td>
+<td><a href='delete.php?C_ID={$row['C_ID']}'>DELETE</a></td>
+</tr>
+";
+}
